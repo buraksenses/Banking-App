e1c6aad54e24842ab3b6faa3260422b3bd666565
Burak Şenses <buraksenseees@gmail.com>
2023-12-26 03:15:39 +0300

    ApplicationServiceExtensions class

---
+++ BankProject/BankProject.API/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,18 @@
+﻿using BankProject.Data.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace BankProject.API.Extensions;
+
+public static class ApplicationServiceExtensions
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
+    {
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+
+        services.AddDbContext<BankDbContext>(options =>
+            options.UseSqlServer(config.GetConnectionString("BankConnectionString")));
+        
+        return services;
+    }
+}
--- BankProject/BankProject.API/Program.cs
+++ BankProject/BankProject.API/Program.cs
@@ -1,17 +1,12 @@
-using BankProject.Data.Context;
-using Microsoft.EntityFrameworkCore;
+using BankProject.API.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
-builder.Services.AddDbContext<BankDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("BankConnectionString")));
+builder.Services.AddApplicationServices(builder.Configuration);
 
 var app = builder.Build();
 
